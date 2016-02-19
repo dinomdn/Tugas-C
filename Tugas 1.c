bcde9190a223b87ad98263a586aaf8f50377c6cf
dinomdn <dinomdn@gmail.com>
2016-02-19 16:51:10 +0700

    Update Tugas 1.c

--- Tugas 1.c
+++ Tugas 1.c
@@ -1,58 +1,17 @@
 #include <stdio.h>
 
-int main ()
-{
-    int w,x;
+int main(){
+	int x,y;
 	char a,b;
 	int p;
-	printf("Dino Ramadhan\n");
-	printf("14.111.2249\n");
 	printf("Demo Struktur Kondisi\n");
 	printf("----------------------\n");
 	printf("Masukkan bilangan asli 1 : ");
 	scanf("%s",&a);
-	w =a;
-	switch(w)
+	x =a;
+	switch(x)
     		{
     		case 48:
-			w = 0;
-			break;
-			case 49:
-				w = 1;
-			break;
-			case 50:
-				w = 2;
-			break;
-			case 51:
-				w = 3;
-			break;
-			case 52:
-				w = 4;
-			break;
-			case 53:
-				w = 5;
-			break;
-			case 54:
-				w = 6;
-			break;
-			case 55:
-				w = 7;
-			break;
-			case 56:
-				w = 8;
-			break;
-			case 57:
-				w = 9;
-			break;
-			default:
-				w = 0;
-			}
-	printf("Masukkan bilangan asli 2 : ");
-	scanf("%s",&b);
-	x =b;
-			switch (x)
-			{
-			case 48:
 				x = 0;
 			break;
 			case 49:
@@ -83,9 +42,47 @@ int main ()
 				x = 9;
 			break;
 			default:
-				x= 0;
+				x = 0;
 			}
-					if(w!=0 && x!=0)
+	printf("Masukkan bilangan asli 2 : ");
+	scanf("%s",&b);
+	y =b;
+			switch (y)
+			{
+			case 48:
+				y = 0;
+			break;
+			case 49:
+				y = 1;
+			break;
+			case 50:
+				y = 2;
+			break;
+			case 51:
+				y = 3;
+			break;
+			case 52:
+				y = 4;
+			break;
+			case 53:
+				y = 5;
+			break;
+			case 54:
+				y = 6;
+			break;
+			case 55:
+				y = 7;
+			break;
+			case 56:
+				y = 8;
+			break;
+			case 57:
+				y = 9;
+			break;
+			default:
+				y= 0;
+			}
+					if(x!=0 && y!=0)
 						{
 						printf("\n\nJenis Operator\n");
 						printf("--------------------\n");
@@ -99,35 +96,35 @@ int main ()
 								case 1:
 									printf("\nContoh Operator Aritmatika\n");
 									printf("-----------------------------\n");
-									printf("Penjumlahan \t: %d + %d = %d \n",w,x,w+x);
-									printf("Perkalian \t: %d x %d = %d \n",w,x,w*x);
+									printf("Penjumlahan \t: %d + %d = %d \n",x,y,x+y);
+									printf("Perkalian \t: %d x %d = %d \n",x,y,x*y);	
 								break;
-
+								
 									case 2:
 									printf("\nContoh Operator Relasional\n");
 									printf("-----------------------------\n");
-									printf("Persamaan \t: %d == %d = ",w,x);
-										if(w==x)
+									printf("Persamaan \t: %d == %d = ",x,y);
+										if(x==y)
 											printf("True\n");
 										else
 											printf("false\n");
-
-									printf("Pertidaksamaan \t: %d != %d = ",w,x);
-										if(w!=x)
+											
+									printf("Pertidaksamaan \t: %d != %d = ",x,y);
+										if(x!=y)
 											printf("True");
 										else
-											printf("false");
+											printf("false");	
 								break;
 									case 3:
 										printf("\nContoh Operator Logika\n");
-										printf("Logika AND \t= %d && %d = %d\n",w,x,w&&x);
-										printf("Logika OR \t= %d || %d = %d\n",w,x,w||x);
+										printf("Logika AND \t= %d && %d = %d\n",x,y,x&&y);
+										printf("Logika OR \t= %d || %d = %d\n",x,y,x||y);
 									break;
-
+								
 								default:
 									printf("pilihan tidak valid");
 							}
-
+						
 						}
 					else
 						printf("\nmasukan tidak valid");
